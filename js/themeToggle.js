a9bc6982bc85c8984ae2202a497bcb66df927ee4
Matthieu Chatelin <matthieu888@gmail.com>
2025-03-24 15:18:00 +0100

    Add toggle day/night mode & language switch fr/en

--- js/themeToggle.js
+++ js/themeToggle.js
@@ -1,1 +1,30 @@
 // Mode jour/nuit
+const themeToggle = document.querySelector("#theme-toggle");
+
+const logo = document.querySelector(".logo");
+
+// Fonction pour basculer entre le mode jour et nuit
+function toggleTheme(isDarkMode) {
+  if (isDarkMode) {
+    document.body.classList.add("dark-mode");
+    localStorage.setItem("theme", "dark");
+    logo.src = "./assets/images/logo-white.png";
+  } else {
+    document.body.classList.remove("dark-mode");
+    localStorage.setItem("theme", "light");
+    logo.src = "./assets/images/logo-blue.png";
+  }
+}
+
+// Vérification du thème du localStorage au chargement
+const savedTheme = localStorage.getItem("theme");
+
+// Par défaut, on est en dark mode
+const isDarkMode = savedTheme !== "light";
+
+themeToggle.checked = isDarkMode;
+toggleTheme(isDarkMode);
+
+themeToggle.addEventListener("change", () => {
+  toggleTheme(themeToggle.checked);
+});
--- js/translations.js
+++ js/translations.js
@@ -1,1 +1,43 @@
 // Gestion des langues
+
+// Dictionnaire des traductions
+const translations = {
+  fr: {
+    about: "À PROPOS",
+    skills: "COMPÉTENCES",
+    day: "Jour",
+    night: "Nuit",
+    welcome: "Bienvenue sur mon portfolio",
+  },
+  en: {
+    about: "ABOUT",
+    skills: "SKILLS",
+    day: "Day",
+    night: "Night",
+    welcome: "Welcome to my port folio",
+  },
+};
+
+const toggle = document.querySelector("#lang-toggle");
+
+// Fonction pour switcher la langue
+function changeLanguage(lang) {
+  localStorage.setItem("lang", lang);
+
+  document.querySelectorAll("[data-i18n").forEach((el) => {
+    const key = el.getAttribute("data-i18n");
+    el.textContent = translations[lang][key];
+  });
+
+  toggle.checked = lang === "en";
+}
+
+// Détection de la langue enregistrée
+const currentLang = localStorage.getItem("lang") || "fr";
+changeLanguage(currentLang);
+
+// Evènement sur le switch
+toggle.addEventListener("change", () => {
+  const newLang = toggle.checked ? "en" : "fr";
+  changeLanguage(newLang);
+});
